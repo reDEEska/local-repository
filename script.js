2392e57bf645d7b9fdc7f602ad23c49f18c08a00
reDEEska <flydee@gmail.com>
2022-04-05 07:14:00 +0300

    Day1

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-// Всплывающие окна
+/* Задание на урок:
 
-//const question = confirm('Are you here?')
-// console.log(question); //true or false
-// const answer = prompt('Вам есть 18', 'Уверен?');
-// console.log(answer); //уверен?
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-//Интерполяция (ES6)
-const category = 'toys';
-console.log(`someurl.com/${category}/ hi пишет пробел`);
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-//Операторы
-let incr = 10,
-   decr = 10;
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-incr++; //инкримент увеличивает на 1
-decr--; //декримент уменьшает на 1
+Проверить, чтобы все работало без ошибок в консоли */
 
-console.log(++incr);
-console.log(--decr);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// ! оператор отрицания
-console.log(10 != 8);
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
